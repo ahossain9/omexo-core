136dd596ede05267d1fef23534445e6621a24221
ahossain9 <a93hossain@gmail.com>
2024-07-25 03:33:22 +0600

    Added spining image element

--- base.php
+++ base.php
@@ -92,12 +92,13 @@ class Plugin {
      */
     public function init_widgets() {
 
-        $LMSwidget = [
+        $TutorLMSwidget = [
             'course-grid',
             'course-carousel',
             'category-image-box',
             'category-icon-box',
             'course-search',
+            'dynamic-course-category',
         ];
 
         $widgets = [
@@ -116,6 +117,7 @@ class Plugin {
             'skill-bar',
             'accordian',
             'step-flow',
+            'spinning-image',
             'nav-menu',
             'site-logo',
             'mini-cart',
@@ -123,7 +125,7 @@ class Plugin {
         ];
 
         if ( function_exists('tutor') ) {
-            $widgets = array_merge( $LMSwidget, $widgets);
+            $widgets = array_merge($TutorLMSwidget, $widgets);
         }
 
         foreach ( $widgets as $widget ) {
